70cafbe3a21f8be3eed3acf7dad97a980a3cb5e1
lazaremikaia <121046020+lazaremikaia@users.noreply.github.com>
2023-01-31 23:30:59 +0430

    Create script.js

---
+++ script.js
@@ -0,0 +1,66 @@
+
+
+1
+
+
+for (let i = 0; i < 100 ; i++) {
+    console.log(i)
+
+}
+
+2
+
+let n = 0;
+
+while (n < 50) {
+  n++;
+  console.log(n);
+}
+
+
+
+
+3 
+
+
+let person = ["lazare", "nuca", "giorgi", "levani", "nikusha"] 
+for (let i = 0; i< person.length; i++) {
+    
+    console.log(i ,person[i])
+    
+}
+
+ person.pop()
+ person.push("daviti")
+person.shift()
+ person.unshift("salome")
+ console.log(person)
+
+// let persons = person.slice (0, 3)
+// console.log(persons)
+
+4
+
+
+let numbers = []
+
+for (let index = 1; index < 10000; index++) {
+    numbers.push(index * index)
+    
+}
+
+console.log(numbers)
+
+
+6
+
+let names =["john", 'anna']
+
+
+const getNameTolowerCase = (arr) => {
+for (let index = 0; index < names.length; index++) {
+    console.log (`my name is `)
+    
+}
+
+}
